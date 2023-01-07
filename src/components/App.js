12fd111a1f9d9c04906cea6163e4feb7f0d3beb3
namratanakhale <namratanakhale91@gmail.com>
2023-01-07 21:33:05 +0530

    changes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import "../styles/App.css";
 
 const App = () => {
   //Write your code here
-}
+  const [queryParams] = useSearchParams();
+  const name = queryParams.get("name");
+  const id = queryParams.get("id");
+  const designation = queryParams.get("designation");
+
+  return (
+    <div className="details">
+      Employee {id} named {name} works as {designation}
+    </div>
+  );
+};
 
 export default App;
